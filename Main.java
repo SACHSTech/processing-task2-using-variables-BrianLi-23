bb6887b4ed91e20363b55db153b39e4be8c34fa0
BrianLi23 <animeshack123@gmail.com>
2021-12-14 12:14:13 -0500

    Final Commit (Finished)

--- Main.java
+++ Main.java
@@ -2,15 +2,15 @@ import processing.core.PApplet;
 
 /**
  * Main class to execute sketch
- * @author 
- *
+ * Brian Li
+ * Processing Task 2
  */
 class Main {
   public static void main(String[] args) {
-    
-    String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();
-	  PApplet.runSketch(processingArgs, mySketch);
+
+    String[] processingArgs = { "MySketch" };
+    Sketch mySketch = new Sketch();
+    PApplet.runSketch(processingArgs, mySketch);
   }
-  
+
 }
